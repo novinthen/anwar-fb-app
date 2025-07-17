5f9df381a26bb394ee89dc5dd07038a1fcdb2d0b
novinthen <42584998+novinthen@users.noreply.github.com>
2025-07-17 22:27:46 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -121,7 +121,6 @@ const App = () => {
     useEffect(() => {
         generateFacebookPost();
     }, []);
-
 return (
  <> {/* ADD THIS LINE: Opening React Fragment */}
          <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex items-center justify-center p-4 font-sans">
